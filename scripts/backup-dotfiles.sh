73fb6c09a3333b9127d002a9c36258fb82a4bb2b
NateWeiler <nateweiler84@gmail.com>
2021-03-03 14:59:07 -0500

    a

--- scripts/backup-dotfiles.sh
+++ scripts/backup-dotfiles.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 
 cd /sdcard/github/
-echo "Cloning dotfiles Repo"
+echo "\nCloning dotfiles Repo\n"
 git clone https://github.com/NateWeiler/dotfiles.git
-cd dotfiles
-echo "\Removing old Note-Termux Folder"
+cd /sdcard/github/dotfiles/
+echo "\nRemoving old Note-Termux Folder\n"
 rm -rf Note-Termux/
 mkdir -p Note-Termux
-cd Note-Termux/
+mkdir -p Note-Termux/backups
+touch Note-Termux/backups/.gitkeep
+cd /data/data/com.termux/files/home/
 echo "\nZipping Note-Termux.zip"
-zip -r -q Note-Termux.zip ~/Mail ~/Optiva-Framework ~/aliases ~/apktool ~/bin ~/buildAPKs ~/builtAPKs ~/codiad ~/crontab-testing ~/crontab.txt ~/data ~/debian-testing ~/deboot_debian ~/debootstrap ~/etc ~/export ~/files ~/functions ~/go ~/kali-fs ~/kali.sh ~/metasploit-framework ~/neofetch ~/oh-my-fish ~/pad.txt ~/prefix ~/progs ~/routersploit ~/start-kali.sh ~/start-ubuntu.sh ~/termux-desktop ~/termux.properties ~/tmp ~/ubuntu-fs ~/ubuntu.sh ~/xerxes
+zip -r -q Note-Termux.zip Mail Optiva-Framework aliases apktool bin buildAPKs builtAPKs codiad crontab-testing crontab.txt data debian-testing deboot_debian debootstrap etc export files functions go kali-fs kali.sh metasploit-framework neofetch oh-my-fish pad.txt prefix progs routersploit start-kali.sh start-ubuntu.sh termux-desktop termux.properties tmp ubuntu-fs ubuntu.sh xerxes
 echo "\nUnzipping Note-Termux.zip\n"
-unzip -qq Note-Termux.zip
-mv -t /sdcard/github/dotfiles/Note-Termux/ data/data/com.termux/files/home/* data/data/com.termux/files/home/.*
+unzip -qq Note-Termux.zip -d /sdcard/github/dotfiles/Note-Termux/
 echo "\nRemoving Note-Termux.zip\n"
-rm -rf Note-Termux.zip data/data/
+rm -rf Note-Termux.zip
+cd /sdcard/github/dotfiles/Note-Termux/
 find . -depth -type d -name ".git" -exec rm -rf {} \;
-find . -depth -type d -name ".github" -exec rm -rf {} \;
-cd ..
-echo "\nAdding files to Commit"
-git add -Af
+find . -depth -type d -name ".github" -exec rm -rf {}
+\;
+find . -depth -type f -name "desktop.ini" -exec rm -rf {} \;
+find . -depth -type f -name "DS_Store" -exec rm -rf {} \;
+cd /sdcard/github/dotfiles/
+echo "\nAdding files to Commit\n"
+git add -A
 echo "\nCommitting files to Repo\n"
 git commit -m "Note Termux"
 echo "\nPushing files to Repo with Force\n"
-git push -u --force origin master
+git push --force
 git status
 cd /sdcard/github/
 
