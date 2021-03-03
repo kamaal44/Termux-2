2cb758aa7f32a626e3966922673d944e7c273e25
NateWeiler <nateweiler84@gmail.com>
2021-03-03 10:41:27 -0500

    a

---
+++ python/tts/pyttsx3/example/say.py
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/python3
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("hello crazy programmer")
+engine.setProperty('rate',120)
+engine.setProperty('volume', 0.9)
+engine.runAndWait()
